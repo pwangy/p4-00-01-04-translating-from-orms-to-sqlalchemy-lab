2d01b48390631fa355d5842b973ea57281f01543
pwangy <blipsandclicks@gmail.com>
2024-03-31 16:17:11 -0700

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -7,7 +7,17 @@ Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
+    
+    # Index('index_name', 'id')
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __repr__(self):
+        return f"""
+            <Dog #{self.id}:
+                Name: {self.name},
+                Breed: {self.breed}
+            >
+        """
